fa2c18708e92717d7777a9943d0cc9937cdfccd9
Ferl Jhon D. Javier <103488259+1goadmin@users.noreply.github.com>
2022-04-20 13:58:40 +0800

    Header Update
    Performance Test
    
    Co-Authored-By: Ferl Jhon D. Javier <80177329+ferlerrorr@users.noreply.github.com>

--- wp-content/themes/onetheme/header.php
+++ wp-content/themes/onetheme/header.php
@@ -6,16 +6,21 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="<?=bloginfo('Description');?>">
 <title><?=bloginfo('name');?> | <?= is_front_page()? bloginfo('description'): wp_title('');?></title>
-<!-- <link rel="stylesheet" href="style.min.css"> -->
-	<!-- Font Awesome -->
-	<script src="https://kit.fontawesome.com/a5b3b870d7.js" crossorigin="anonymous"></script>
-	<!-- Font Awesome -->
-  <!-- jquery -->
-  <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
-  <script src="https://kenwheeler.github.io/slick/slick/slick.js"></script>
-  <link href="https://file.myfontastic.com/sK9sqngFqBGZKZUVm4zSJ3/icons.css" rel="stylesheet">
-  <!-- jquery -->
+<!-- Pre-connects -->
+<link rel="preconnect" href="https://kit.fontawesome.com/">
+<link rel="preconnect" href="https://kenwheeler.github.io/slick/slick/">
+<link rel="preconnect" href="https://file.myfontastic.com/sK9sqngFqBGZKZUVm4zSJ3/">
+<!--  Pre-connects -->
+<!-- jquery -->
+<script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
+<!-- jquery -->
+<!-- Slick -->
+<script src="https://kenwheeler.github.io/slick/slick/slick.js"></script>
+<!-- Slick -->
 <?php wp_head();?>
+
+<script src="https://kit.fontawesome.com/a5b3b870d7.js" crossorigin="anonymous"></script>
+<link href="https://file.myfontastic.com/sK9sqngFqBGZKZUVm4zSJ3/icons.css" rel="stylesheet">
 <meta name="description" content="We provide an end-to-end solution for brands to do e-commerce business. Take your online selling activities to the next level by leveraging on our integrated technologies to give you seamless and efficient operations.">
 </head>
 <body>
